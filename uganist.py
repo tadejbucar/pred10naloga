5e2882799293bab895b1f45a7ccd6faa37bca361
tadejbucar <35562090+tadejbucar@users.noreply.github.com>
2018-02-24 13:54:43 +0100

    Delete uganist.py

--- uganist.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-secret = 10
-guess = int(raw_input("Skrito število je: "))
-if guess == secret:
-    print "Bravo! Uganil/a si pravo število!☺"
-else:
-    print "Auč, poskusi znova! ☹"
